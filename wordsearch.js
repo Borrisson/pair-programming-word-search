cec881c8f2e836c21839090a97b99ef67bc472fa
Borrisson <lucborris@gmail.com>
2021-01-13 21:43:21 +0000

    using transpose we could solve this problem

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const verticalArray = transpose(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let x of verticalArray) {
+    if (x.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let output = [];
+    
+  for (const row in matrix[0]) { //0,1
+    let arr = [];
+    for (const col in matrix) { //0,1,2
+      arr.push(matrix[col][row]);
+    }
+    output.push(arr);
+  }
+  
+  return output;
+    
+};
+module.exports = wordSearch;
